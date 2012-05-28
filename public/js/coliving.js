32d07c25f00e23a447f65ecb091b83a885121910
Jessy Kate Schingler <jessy@jessykate.com>
2012-05-28 16:59:44 -0700

    er, syntax error

--- public/js/coliving.js
+++ public/js/coliving.js
@@ -26,14 +26,19 @@ $(function() {
 		},
 
 		formSubmit: function(e) {
+			console.log("in formSubmit");
 			h = new House();
+			fields = {}
 			// retrieve the form fields and populate a new House model with the info. 
-			$('form input, form textarea').each(function(i) {
-				//if ( $(this).is(":checked") ) {
-				if ( $(this).type( $(this).val() ) {
-					console.log($(this).attr("name") + ":" + $(this).val());
+			$('form input, form textarea').each(function() {
+				if ( $(this).is(":checked") ) {
+					fields[$(this).attr("name")] = true;
+				} else if ( !$(this).is("input[type=checkbox]") && $(this).val() ) {
+					fields[$(this).attr("name")] = $(this).val();
 				}
-			})
+			});
+			console.log(fields);
+
 			// save the model
 			// reload the map with the new house
 			e.preventDefault();
