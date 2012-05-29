f55f93937bd9f66013f21f3b9934732a8343d810
Jessy Kate Schingler <jessy@jessykate.com>
2012-05-28 22:44:19 -0700

    putting more of the basics in place. map almost working. no persistence or styling yet

--- public/js/coliving.js
+++ public/js/coliving.js
@@ -1,8 +1,29 @@
 $(function() {
 
+	//
+	Backbone.sync = function(method, model, options) {
+		return 0;
+	};
+	
 	/////////////////////// MODELS ////////////////////////
 
 	window.House = Backbone.Model.extend({
+		initialize: function() {
+			this.geocoder = new google.maps.Geocoder();
+		},
+
+		geocodeAddr: function() {
+			var self = this;
+			this.geocoder.geocode( { 'address': this.get("address")}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+				latStr = results[0].geometry.location.lat().toString();
+				lngStr = results[0].geometry.location.lng().toString();
+				self.set({latLong: latStr + "," + lngStr });
+				} else {
+					console.log("Geocode was not successful for the following reason: " + status);
+				}
+			});
+		}
 	});
 
 	window.Houses = Backbone.Collection.extend({
@@ -12,7 +33,20 @@ $(function() {
 
 	/////////////////////// VIEWS ////////////////////////
 	
-	window.Signup = Backbone.View.extend({
+	window.ListingView = Backbone.View.extend({
+		initialize: function() {
+			this.template = _.template($("#listing-template").html());
+		},
+
+		render: function() {
+			console.log("rendering house listing");
+			$(this.el).html(this.template(this.model.toJSON()));
+			return this;
+		}
+
+	});
+
+	window.NewHouse = Backbone.View.extend({
 		events: {'click #form-submit': 'formSubmit'},
 	
 		initialize: function() {
@@ -20,14 +54,36 @@ $(function() {
 			//this.houses.fetch();
 
 			this.template = _.template($("#signup-template").html());
-			_.bindAll(this, 'render');
-			this.houses.bind('add', this.addOne);
+			_.bindAll(this, 'render', 'addHouse');
+			this.houses.bind('add', this.addHouse);
 		},
 
 		render: function() {
 			console.log("rendering signup form");
 			$(this.el).html(this.template({}));
 			return this;
+		},
+
+		renderMap: function() {
+			console.log("in renderMap()");
+			map = new OpenLayers.Map("mainmap");
+			map.addLayer(new OpenLayers.Layer.OSM());
+			var zoom=16;
+			
+			var markers = new OpenLayers.Layer.Markers( "Markers" );
+			map.addLayer(markers);
+
+
+			var locations = this.houses.pluck("latLong");
+			console.log(locations);
+			locations.forEach(function(ll) {
+				ll = ll.split(",");
+				latitude = parseFloat(ll[0]);
+				longitude = parseFloat(ll[1]);
+				markers.addMarker(new OpenLayers.Marker(
+					new OpenLayers.LonLat(latitude, longitude)
+				));
+			});
 		},
 
 		formSubmit: function(e) {
@@ -43,16 +99,25 @@ $(function() {
 				}
 			});
 			console.log(newhouse);
-			//
+			
 			// save a new model to the collection, which will also trigger the
-			// 'add' event on the collection. 
-			this.houses.create(newhouse)
+			// 'add' event on the collection and the model. 
+			houseModel = this.houses.create(newhouse);
+			// geocode the address
+			// TODO this should probably be set up as an event that
+			// automatically triggers on creation.
+			houseModel.geocodeAddr();
+			console.log("lat and long (might not have returned yet):");
+			console.log(houseModel.get("latLong"));
 			e.preventDefault();
 		},
 
-		addOne: function(house) {
-			// reload the map with the new house
-			// var houseListing = new HouseListingView(house);
+		addHouse: function(house) {
+			// reload the map with the new house and display the house listing
+			// below the map
+			var listing = new ListingView({model:house});
+			$("#content").html(listing.render().el);
+			this.renderMap();
 		}
 	});
 
@@ -65,8 +130,8 @@ $(function() {
 		},
 
 		home: function() {
-			signup = new Signup({el:$("#content")});
-			signup.render();
+			newhouse = new NewHouse({el:$("#content")});
+			newhouse.render();
 		}
 
 	});
