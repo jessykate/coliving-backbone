8421cdc395cc3ac46839c280311771310e9a6991
Jessy Kate Schingler <jessy@jessykate.com>
2012-06-02 23:30:15 -0700

    refactoring + table listing for houses

--- public/js/coliving.js
+++ public/js/coliving.js
@@ -2,6 +2,7 @@ $(function() {
 
 	// boostrap a list of existing houses. 
 	// TODO (jessykate) retrieve real list from server
+	/*
 	bootstrapHouses = [
 		{address: "21677 rainbow drive, cupertino, ca, 95014", bedrooms: 8, name:"Rainbow Mansion", 
 			contact: "zwexlerberon@gmail.com", website: "http://rainbowmansion.com", latLong: "37.300079,-122.05498899999998",
@@ -11,13 +12,25 @@ $(function() {
 	];
 
 	// fake the saving while we don't have a persistence layer. 
-	Backbone.sync = function(method, model, options) {
-		return 0;
+	//Backbone.sync = function(method, model, options) {
+	//	return 0;
+	//};
+	*/
+	/////////////////////// UTIL ////////////////////////
+	locationParse = function(locString) {
+		// returns a list [longitude, latitude] (in that order!)
+		ll = locString.split(",");
+		latitude = parseFloat(ll[0]);
+		longitude = parseFloat(ll[1]);
+		loc = [longitude, latitude];
+		return loc;
 	};
-	
+
 	/////////////////////// MODELS ////////////////////////
 
 	window.House = Backbone.Model.extend({
+		idAttribute: "_id",
+
 		initialize: function() {
 			this.geocoder = new google.maps.Geocoder();
 		},
@@ -39,10 +52,16 @@ $(function() {
 
 	window.Houses = Backbone.Collection.extend({
 		model: House,
-		url: "/houses"
+		url: "/api/houses"
 	});
 
 	/////////////////////// VIEWS ////////////////////////
+
+	// houses needs to be declared before the objects below refer to it, since
+	// javascript is evaluated immediately. 
+	// XXX (jessykate) why does this not work when using "this"?
+	window.houses = new Houses();
+	houses.reset(bootstrapHouses);
 	
 	window.PreviewView = Backbone.View.extend({
 		initialize: function() {
@@ -70,87 +89,21 @@ $(function() {
 
 	});
 
-	window.NewHouse = Backbone.View.extend({
+	window.NewHouseView = Backbone.View.extend({
 		events: {'click #form-submit': 'formSubmit',
 				'click #form-confirm': 'confirmSubmit'	
 		},
-	
-		initialize: function() {
-			this.houses = new Houses();
-			this.houses.reset(bootstrapHouses);
 
+		initialize: function(options) {
+			this.collection = houses;
 			this.template = _.template($("#signup-template").html());
-			_.bindAll(this, 'render', 'preview', 'houseListingPreview', 'confirmSubmit', 'formSubmit', 'renderMap');
-			this.houses.bind('add', this.showListings);
-			this.renderMap();
+			_.bindAll(this, 'render', 'preview', 'formSubmit', 'updateMap', 'confirmSubmit');
 		},
 
 		render: function() {
-			console.log("rendering signup form");
-			$(this.el).html(this.template({}));
+			console.log("rendering new house form");
+			$(this.el).html(this.template());
 			return this;
-		},
-
-		locationParse: function(locString) {
-			// returns a list [longitude, latitude] (in that order!)
-			ll = locString.split(",");
-			latitude = parseFloat(ll[0]);
-			longitude = parseFloat(ll[1]);
-			loc = [longitude, latitude];
-			return loc;
-		},
-
-		renderMap: function() {
-			console.log("in renderMap()");
-			map = new OpenLayers.Map("mainmap");
-			//map.addLayer(new OpenLayers.Layer.OSM("New Layer", "http://otile1.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.png"));
-			map.addLayer(new OpenLayers.Layer.OSM());
-			var markerLayer = new OpenLayers.Layer.Markers( "Markers" );
-			map.addLayer(markerLayer);
-
-			var locations = this.houses.pluck("latLong");
-			self = this;
-			locations.forEach(function(ll) {
-				loc = self.locationParse(ll);
-				var lonlat = new OpenLayers.LonLat(loc[0], loc[1]).transform(
-					new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-					new OpenLayers.Projection("EPSG:900913")
-					//map.getProjectionObject() // to Spherical Mercator Projection
-				);
-				markerLayer.addMarker(new OpenLayers.Marker(lonlat));
-			});
-
-			this.map = map;
-			this.markerLayer = markerLayer;
-
-			this.map.updateCenter = function(locStr, zoom, that) {
-				var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-				var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-				// center the map somewhere in the middle of the world
-				loc = that.locationParse(locStr)
-				var mapCenter = new OpenLayers.LonLat(loc[0], loc[1]).transform( fromProjection, toProjection);
-				// forces the tiles of the map to render. 
-				this.setCenter(mapCenter, zoom);
-			}
-
-			var zoom = 2;
-			this.map.updateCenter("33.137551, -163.476563", zoom, this);
-		},
-
-		// update the map as a callback when a model latLong attribute changes.
-		updateMap: function(model) {
-			loc = this.locationParse(model.get("latLong"));
-			var lonlat = new OpenLayers.LonLat(loc[0], loc[1]).transform(
-				new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-				new OpenLayers.Projection("EPSG:900913")
-				//map.getProjectionObject() // to Spherical Mercator Projection
-			);
-			this.markerLayer.addMarker(new OpenLayers.Marker(lonlat));
-
-			// re-render the map, slightly zoomed in around the area of the
-			// newly added house
-			var zoom = 12;
-			this.map.updateCenter(model.get("latLong"), zoom, this);
 		},
 
 		houseFromForm: function(e) {
@@ -173,13 +126,19 @@ $(function() {
 			this.updateMap(model);
 		},
 
+		houseListingPreview: function(house) {
+			// display the house listing below the map. triggered by the add event on the collection. 
+			var pv = new PreviewView({model:house});
+			$("#content").html(pv.render().el);
+		},
+
 		formSubmit: function(e) {
 			console.log("in formSubmit");
 			houseAttr = this.houseFromForm(e);
 			console.log(houseAttr);
 			
-			// create a model to store the object attributes, 
-			// house = this.houses.create(newhouse);
+			// create a model to store the object attributes and trigger the
+			// geocoding
 			house = new House(houseAttr);
 			house.bind('change:latLong', this.preview, this);
 			
@@ -187,8 +146,23 @@ $(function() {
 			// the latLong attribute has successfully been set on the House
 			// model.
 			house.geocodeAddr();
-
 			e.preventDefault();
+		},
+
+		// update the map as a callback when a model latLong attribute changes.
+		updateMap: function(model) {
+			loc = locationParse(model.get("latLong"));
+			var lonlat = new OpenLayers.LonLat(loc[0], loc[1]).transform(
+				new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+				new OpenLayers.Projection("EPSG:900913")
+				//map.getProjectionObject() // to Spherical Mercator Projection
+			);
+			markerLayer.addMarker(new OpenLayers.Marker(lonlat));
+
+			// re-render the map, slightly zoomed in around the area of the
+			// newly added house
+			var zoom = 12;
+			map.updateCenter(model.get("latLong"), zoom, this);
 		},
 
 		confirmSubmit: function(e) {
@@ -196,15 +170,67 @@ $(function() {
 			// the 'add' event on the collection and the model. 
 			houseAttr = this.houseFromForm(e);
 			console.log("house attributes after confirm.");
-			console.log(houseAttr);
-			house = this.houses.create(houseAttr);
+			h = this.collection.create(houseAttr);
+			console.log(h);
+			app.navigate("/", {trigger:true});
+			e.preventDefault();
+		}
+
+	});
+
+	window.AppView = Backbone.View.extend({
+		events: {'click #add-house': 'newHouseHandler',
+			'click tr.row-link': 'houseDisplayHandler'
+		},
+	
+		initialize: function(options) {
+			this.collection = houses;
+
+			this.template = _.template($("#listings-template").html());
+			_.bindAll(this, 'render', 'newHouseHandler', 'renderMap');
+			this.renderMap();
+		},
+
+		render: function() {
+			$(this.el).html(this.template({houses: this.collection.toJSON()}));
+			return this;
+		},
+
+		houseDisplayHandler: function(e) {
+			console.log("in houseDisplayHandler");
+		},
+
+		newHouseHandler: function(e) {
+			app.navigate("/new", {trigger:true});
 			e.preventDefault();
 		},
 
-		houseListingPreview: function(house) {
-			// display the house listing below the map. triggered by the add event on the collection. 
-			var pv = new PreviewView({model:house});
-			$("#content").html(pv.render().el);
+		renderMap: function() {
+			console.log("in renderMap()");
+
+			var locations = this.collection.pluck("latLong");
+			locations.forEach(function(ll) {
+				loc = locationParse(ll);
+				var lonlat = new OpenLayers.LonLat(loc[0], loc[1]).transform(
+					new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+					new OpenLayers.Projection("EPSG:900913")
+					//map.getProjectionObject() // to Spherical Mercator Projection
+				);
+				markerLayer.addMarker(new OpenLayers.Marker(lonlat));
+			});
+
+			map.updateCenter = function(locStr, zoom, that) {
+				var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+				var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+				// center the map somewhere in the middle of the world
+				loc = locationParse(locStr)
+				var mapCenter = new OpenLayers.LonLat(loc[0], loc[1]).transform( fromProjection, toProjection);
+				// forces the tiles of the map to render. 
+				this.setCenter(mapCenter, zoom);
+			}
+
+			var zoom = 2;
+			map.updateCenter("33.137551, -163.476563", zoom, this);
 		},
 
 		showListings: function(model, coll) {
@@ -216,17 +242,40 @@ $(function() {
 	/////////////////////// ROUTER ////////////////////////
 	
 	var AppRouter = Backbone.Router.extend({
+		// remember you can't visit urls directly unless you have the server
+		// resonding to a request at this url. 
 
 		routes: {
-			"": "home"
+			"": "home",
+			"new": "newHouse",
+			"house/:houseid" : "showHouse"
 		},
 
+		initialize: function() {
+		},
+		
 		home: function() {
-			newhouse = new NewHouse({el:$("#content")});
-			newhouse.render();
+			console.log(this.houses);
+			appview = new AppView({el:$("#content"), collection: houses});
+			appview.render();
+		},
+
+		newHouse: function() {
+			houseform = new NewHouseView({el: $("#content"), collection: houses});
+			houseform.render();
+		},
+
+		showHouse: function() {
+
 		}
 
 	});
+
+	// make the houses collection and map available to all routes and views
+	window.map = new OpenLayers.Map("mainmap");
+	map.addLayer(new OpenLayers.Layer.OSM());
+	window.markerLayer = new OpenLayers.Layer.Markers( "Markers" );
+	map.addLayer(markerLayer);
 
 	app = new AppRouter();
 	Backbone.history.start({pushState:true});
@@ -234,13 +283,15 @@ $(function() {
 })
 
 // TODO list
-// home view should show listings
-// click on and view individual links. push state for individual house ids
+// + houses list should persist after return to home view
 // persistent storage
+// house edit - use oath?
+// click on and view individual houses. push state for individual house ids
+// different color for the location most recently submitted.
+// map markers should have basic house info and link
+// recapcha
 // gmaps search-ahead
 // bootstrap from server 
-// quicksearch? display/filter/search (update map correspondingly)
-// different color for the location most recently submitted.
+// use data tables to display and search house listings
 // form validation - required fields, url, email, 
 // open layers form control - make less fugly
-// map markers should have basic house info and link
